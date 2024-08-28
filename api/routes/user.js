d6f73cc2047626360ee420dafac5c148c628b04b
kaleem ullah <51668264+kalemullah@users.noreply.github.com>
2024-08-28 21:06:08 +0500

    bcyptjs pakg added

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const userModel=require('../model/user_model');
 const mongoose=require('mongoose');
-const bcrypt=require('bcrypt'); 
+const bcrypt=require('bcryptjs'); 
 const jwt=require('jsonwebtoken')
 
 router.get('/', (req, res, next) => {
